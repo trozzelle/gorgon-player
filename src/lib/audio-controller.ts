4c26c7132c4229c97a3cb02931664a0cd67e13d8
Torin Rozzelle <torinadamrozzelle@gmail.com>
2025-01-27 16:06:53 -0500

    Blocked out basic class for two sources

--- src/lib/audio-controller.ts
+++ src/lib/audio-controller.ts
@@ -39,5 +39,63 @@ export class AudioController {
         this.gainB.connect(this.audioContext.destination)
     }
 
+    async loadTracks(trackAUrl: string, trackBUrl: string): Promise<void> {
+        const [bufferA, bufferB] = await Promise.all([
+            this.fetchAudioBuffer(trackAUrl),
+            this.fetchAudioBuffer(trackBUrl)
+        ])
+
+        this.audioBufferA = bufferA
+        this.audioBufferB = bufferB
+    }
+
+    private async fetchAudioBuffer(url: string): Promise<AudioBuffer> {
+        /**
+         * Fetches and decodes track
+         */
+        const response = await fetch(url)
+        const arrayBuffer = await response.arrayBuffer()
+        return await this.audioContext.decodeAudioData(arrayBuffer)
+    }
+
+    play() {
+        // If we're already playing or either buffer is empty, return
+        if (this.isPlaying) return
+        if (!this.audioBufferA || !this.audioBufferB) return
+
+        this.sourceA = this.audioContext.createBufferSource()
+        this.sourceB = this.audioContext.createBufferSource()
+
+
+        this.sourceA.buffer = this.audioBufferA
+        this.sourceB.buffer = this.audioBufferB
+
+        this.sourceA.connect(this.gainA)
+        this.sourceB.connect(this.gainB)
+
+        this.startTime = this.audioContext.currentTime - this.offset
+        this.sourceA.start(0, this.offset)
+        this.sourceB.start(0, this.offset)
+
+        this.isPlaying = true
+
+    }
+
+    pause() {
+        if (!this.isPlaying) return
+
+        this.sourceA?.stop()
+        this.sourceB?.stop()
+
+        this.offset = this.audioContext.currentTime - this.startTime
+        this.isPlaying = false
+    }
+
+    setBalance(value: number) {
+        this.gainA.gain.value = 1 - value
+        this.gainB.gain.value = value
+    }
+
+
 
 }
