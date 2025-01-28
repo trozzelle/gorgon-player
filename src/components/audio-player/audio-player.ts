6ef8923b36832ffdf162556b407f5f2aac2739e5
Torin Rozzelle <torinadamrozzelle@gmail.com>
2025-01-27 23:19:38 -0500

    Adds prettier and eslint Formats entire project

--- src/components/audio-player/audio-player.ts
+++ src/components/audio-player/audio-player.ts
@@ -1,94 +1,93 @@
-import { AudioController} from "../../lib/audio-controller.ts"
+import { AudioController } from '../../lib/audio-controller.ts'
 import styles from './audio-player.css?inline'
 
 export class GorgonPlayer extends HTMLElement {
-    private shadow: ShadowRoot
-    private audioController: AudioController
-    private isPlaying = false
+  private shadow: ShadowRoot
+  private audioController: AudioController
+  private isPlaying = false
 
-    // Store references to our dom elements
-    private playButton: HTMLButtonElement | null = null
-    private slider: HTMLInputElement | null = null
+  // Store references to our dom elements
+  private playButton: HTMLButtonElement | null = null
+  private slider: HTMLInputElement | null = null
 
-    constructor() {
-        super()
-        // TODO: Create dev/prod value insertion here
-        this.shadow = this.attachShadow({mode: 'open'})
-        this.audioController = new AudioController()
+  constructor() {
+    super()
+    // TODO: Create dev/prod value insertion here
+    this.shadow = this.attachShadow({ mode: 'open' })
+    this.audioController = new AudioController()
 
-        this.handlePlay = this.handlePlay.bind(this)
-        this.handleSlider = this.handleSlider.bind(this)
+    this.handlePlay = this.handlePlay.bind(this)
+    this.handleSlider = this.handleSlider.bind(this)
 
-        this.render()
+    this.render()
 
-        this.playButton = this.shadow.querySelector('.play-button')
-        this.slider = this.shadow.querySelector('.compare-slider')
+    this.playButton = this.shadow.querySelector('.play-button')
+    this.slider = this.shadow.querySelector('.compare-slider')
 
-        this.setupEventListeners()
+    this.setupEventListeners()
+  }
+
+  // Setup and teardown lifecycle methods handled
+  // automatically by the browser
+  async connectedCallback() {
+    const trackA = this.getAttribute('track-a') || ''
+    const trackB = this.getAttribute('track-b') || ''
+
+    try {
+      await this.audioController.loadTracks(trackA, trackB)
+    } catch (error) {
+      console.log(`Error loading tracks: ${error}`)
+    }
+  }
+
+  disconnectedCallback() {
+    this.playButton?.removeEventListener('click', this.handlePlay)
+    this.slider?.removeEventListener('input', this.handleSlider)
+  }
+
+  private handlePlay() {
+    if (this.isPlaying) {
+      this.audioController.pause()
+    } else {
+      this.audioController.play()
+    }
+    this.isPlaying = !this.isPlaying
+    this.updatePlayButton()
+  }
+
+  private handleSlider(e: Event) {
+    const value = parseInt((e.target as HTMLInputElement).value) / 100
+    console.log(value)
+    this.audioController.setBalance(value)
+  }
+
+  private updatePlayButton() {
+    if (this.playButton) {
+      this.playButton.textContent = this.isPlaying ? '⏸' : '▶'
+    }
+  }
+
+  private setupEventListeners() {
+    this.playButton?.addEventListener('click', this.handlePlay)
+    this.slider?.addEventListener('input', this.handleSlider)
+  }
+
+  private render() {
+    // const stylesheet = new CSSStyleSheet()
+    // stylesheet.replaceSync(styles)
+    // this.shadow.adoptedStyleSheets = [stylesheet]
+
+    if (this.shadow.adoptedStyleSheets !== undefined && 'replaceSync' in CSSStyleSheet.prototype) {
+      const stylesheet = new CSSStyleSheet()
+      stylesheet.replaceSync(styles)
+      this.shadow.adoptedStyleSheets = [stylesheet]
+    } else {
+      const styleElement = document.createElement('style')
+      styleElement.textContent = styles
+      this.shadow.appendChild(styleElement)
     }
 
-    // Setup and teardown lifecycle methods handled
-    // automatically by the browser
-    async connectedCallback() {
-        const trackA = this.getAttribute('track-a') || ''
-        const trackB = this.getAttribute('track-b') || ''
-
-        try {
-            await this.audioController.loadTracks(trackA, trackB)
-        }
-        catch (error) {
-            console.log(`Error loading tracks: ${error}`)
-        }
-    }
-
-    disconnectedCallback() {
-        this.playButton?.removeEventListener('click', this.handlePlay)
-        this.slider?.removeEventListener('input', this.handleSlider)
-    }
-
-    private handlePlay() {
-        if (this.isPlaying) {
-            this.audioController.pause()
-        } else {
-            this.audioController.play()
-        }
-        this.isPlaying = !this.isPlaying
-        this.updatePlayButton()
-    }
-
-    private handleSlider(e: Event) {
-        const value = parseInt((e.target as HTMLInputElement).value) / 100
-        console.log(value)
-        this.audioController.setBalance(value)
-    }
-
-    private updatePlayButton() {
-        if (this.playButton) {
-            this.playButton.textContent = this.isPlaying ? '⏸' : '▶'
-        }
-    }
-
-    private setupEventListeners() {
-        this.playButton?.addEventListener('click', this.handlePlay)
-        this.slider?.addEventListener('input', this.handleSlider)
-    }
-
-    private render() {
-        // const stylesheet = new CSSStyleSheet()
-        // stylesheet.replaceSync(styles)
-        // this.shadow.adoptedStyleSheets = [stylesheet]
-
-        if (this.shadow.adoptedStyleSheets !== undefined && 'replaceSync' in CSSStyleSheet.prototype) {
-            const stylesheet = new CSSStyleSheet()
-            stylesheet.replaceSync(styles)
-            this.shadow.adoptedStyleSheets = [stylesheet]
-        } else {
-            const styleElement = document.createElement('style')
-            styleElement.textContent = styles
-            this.shadow.appendChild(styleElement)
-        }
-
-        this.shadow.innerHTML = `
+    this.shadow.innerHTML = `
               <div class="player-container">
                 <div class="controls">
                   <button class="play-button" id="play-button">
@@ -111,62 +110,62 @@ export class GorgonPlayer extends HTMLElement {
                 </div>
               </div>
             `
-    }
+  }
 
-    // private setupEventListeners() {
-    //     // const playButton = this.shadow.querySelector('#' +
-    //     //     'play-button')
-    //     // const slider = this.shadow.querySelector('.compare-slider')
-    //     //
-    //     // playButton?.addEventListener('click', () => {
-    //     //     if (this.isPlaying) {
-    //     //         this.audioController.pause()
-    //     //     } else {
-    //     //         this.audioController.play()
-    //     //     }
-    //     //     console.log("Pressed")
-    //     //     this.isPlaying = !this.isPlaying
-    //     //     this.render()
-    //     // })
-    //     //
-    //     // slider?.addEventListener('input', (e) => {
-    //     //     const value = parseInt((e.target as HTMLInputElement).value) / 100
-    //     //     this.audioController.setBalance(value)
-    //     // })
-    //     const playButton = this.shadow.querySelector('.play-button')
-    //     const slider = this.shadow.querySelector('.compare-slider')
-    //
-    //     if (playButton) {
-    //         const handlePlay = () => {
-    //             console.log('Clicked, current statE: ', this.isPlaying)
-    //
-    //             if (this.isPlaying) {
-    //                 this.audioController.pause()
-    //             } else {
-    //                 this.audioController.play()
-    //             }
-    //             this.isPlaying = !this.isPlaying
-    //             this.render()
-    //             this.setupEventListeners()
-    //         }
-    //
-    //         playButton.removeEventListener('click', handlePlay)
-    //         playButton.addEventListener('click', handlePlay)
-    //     }
-    //
-    //     if (slider) {
-    //         const handleSlider = (e: Event) => {
-    //             const value = parseInt((e.target as HTMLInputElement).value) / 100
-    //             this.audioController.setBalance(value)
-    //         }
-    //
-    //         slider.removeEventListener('input', handleSlider)
-    //         slider.addEventListener('input', handleSlider)
-    //     }
-    // }
-    protected getAudioController(): AudioController {
-        return this.audioController
-    }
+  // private setupEventListeners() {
+  //     // const playButton = this.shadow.querySelector('#' +
+  //     //     'play-button')
+  //     // const slider = this.shadow.querySelector('.compare-slider')
+  //     //
+  //     // playButton?.addEventListener('click', () => {
+  //     //     if (this.isPlaying) {
+  //     //         this.audioController.pause()
+  //     //     } else {
+  //     //         this.audioController.play()
+  //     //     }
+  //     //     console.log("Pressed")
+  //     //     this.isPlaying = !this.isPlaying
+  //     //     this.render()
+  //     // })
+  //     //
+  //     // slider?.addEventListener('input', (e) => {
+  //     //     const value = parseInt((e.target as HTMLInputElement).value) / 100
+  //     //     this.audioController.setBalance(value)
+  //     // })
+  //     const playButton = this.shadow.querySelector('.play-button')
+  //     const slider = this.shadow.querySelector('.compare-slider')
+  //
+  //     if (playButton) {
+  //         const handlePlay = () => {
+  //             console.log('Clicked, current statE: ', this.isPlaying)
+  //
+  //             if (this.isPlaying) {
+  //                 this.audioController.pause()
+  //             } else {
+  //                 this.audioController.play()
+  //             }
+  //             this.isPlaying = !this.isPlaying
+  //             this.render()
+  //             this.setupEventListeners()
+  //         }
+  //
+  //         playButton.removeEventListener('click', handlePlay)
+  //         playButton.addEventListener('click', handlePlay)
+  //     }
+  //
+  //     if (slider) {
+  //         const handleSlider = (e: Event) => {
+  //             const value = parseInt((e.target as HTMLInputElement).value) / 100
+  //             this.audioController.setBalance(value)
+  //         }
+  //
+  //         slider.removeEventListener('input', handleSlider)
+  //         slider.addEventListener('input', handleSlider)
+  //     }
+  // }
+  protected getAudioController(): AudioController {
+    return this.audioController
+  }
 }
 
 customElements.define('gorgon-player', GorgonPlayer)
--- src/lib/audio-controller.ts
+++ src/lib/audio-controller.ts
@@ -1,135 +1,129 @@
 declare global {
-    interface Window {
-        webkitAudioContext: typeof AudioContext
-    }
+  interface Window {
+    webkitAudioContext: typeof AudioContext
+  }
 }
 
 // Determines either standard AudioContext or webkit prefix for Safari
 const AudioContextClass = window.AudioContext || window.webkitAudioContext
 
 export interface LoadedTracks {
-    trackA: AudioBuffer | null
-    trackB: AudioBuffer | null
+  trackA: AudioBuffer | null
+  trackB: AudioBuffer | null
 }
 
 export class AudioController {
+  /**
+   * Controller class for audio player
+   * @private
+   */
+  private audioContext: AudioContext
+
+  /**
+   * Declaring A/B for now but might want to generalize it
+   * into an arbitrary number of sources
+   */
+  private sourceA: AudioBufferSourceNode | null = null
+  private sourceB: AudioBufferSourceNode | null = null
+
+  private gainA: GainNode
+  private gainB: GainNode
+
+  private audioBufferA: AudioBuffer | null = null
+  private audioBufferB: AudioBuffer | null = null
+
+  private isPlaying = false
+  private startTime = 0
+  private offset = 0
+
+  constructor() {
+    try {
+      this.audioContext = new AudioContextClass()
+    } catch (e) {
+      throw new Error('Web Audio API is not supported by this browser')
+    }
+    this.gainA = this.audioContext.createGain()
+    this.gainB = this.audioContext.createGain()
+
+    this.gainA.connect(this.audioContext.destination)
+    this.gainB.connect(this.audioContext.destination)
+  }
+
+  async loadTracks(trackAUrl: string, trackBUrl: string): Promise<void> {
     /**
-     * Controller class for audio player
-     * @private
+     * Load A and B track audio buffers
      */
-    private audioContext: AudioContext
+    console.log('Loading tracks: ', { trackAUrl, trackBUrl })
 
+    const [bufferA, bufferB] = await Promise.all([
+      this.fetchAudioBuffer(trackAUrl),
+      this.fetchAudioBuffer(trackBUrl),
+    ])
+
+    console.log('Buffers loaded: ', { bufferA, bufferB })
+
+    this.audioBufferA = bufferA
+    this.audioBufferB = bufferB
+  }
+
+  protected getLoadedTracks(): LoadedTracks {
+    return {
+      trackA: this.audioBufferA,
+      trackB: this.audioBufferB,
+    }
+  }
+
+  protected getSourceNodes() {
+    return {
+      sourceA: this.sourceA,
+      sourceB: this.sourceB,
+    }
+  }
+
+  private async fetchAudioBuffer(url: string): Promise<AudioBuffer> {
     /**
-     * Declaring A/B for now but might want to generalize it
-     * into an arbitrary number of sources
+     * Fetches and decodes track
      */
-    private sourceA: AudioBufferSourceNode | null = null
-    private sourceB: AudioBufferSourceNode | null = null
+    console.log('Fetching: ', url)
+    const response = await fetch(url)
+    const arrayBuffer = await response.arrayBuffer()
+    return await this.audioContext.decodeAudioData(arrayBuffer)
+  }
 
-    private gainA: GainNode
-    private gainB: GainNode
+  play() {
+    // If we're already playing or either buffer is empty, return
+    if (this.isPlaying) return
+    if (!this.audioBufferA || !this.audioBufferB) return
 
-    private audioBufferA: AudioBuffer | null = null
-    private audioBufferB: AudioBuffer | null = null
+    this.sourceA = this.audioContext.createBufferSource()
+    this.sourceB = this.audioContext.createBufferSource()
 
-    private isPlaying = false
-    private startTime = 0
-    private offset = 0
+    this.sourceA.buffer = this.audioBufferA
+    this.sourceB.buffer = this.audioBufferB
 
-    constructor() {
-        try {
-            this.audioContext = new AudioContextClass()
-        } catch (e) {
-            throw new Error("Web Audio API is not supported by this browser")
-        }
-        this.gainA = this.audioContext.createGain()
-        this.gainB = this.audioContext.createGain()
+    this.sourceA.connect(this.gainA)
+    this.sourceB.connect(this.gainB)
 
-        this.gainA.connect(this.audioContext.destination)
-        this.gainB.connect(this.audioContext.destination)
-    }
+    this.startTime = this.audioContext.currentTime - this.offset
+    this.sourceA.start(0, this.offset)
+    this.sourceB.start(0, this.offset)
 
-    async loadTracks(trackAUrl: string, trackBUrl: string): Promise<void> {
-        /**
-         * Load A and B track audio buffers
-         */
-        console.log('Loading tracks: ', { trackAUrl, trackBUrl })
+    this.isPlaying = true
+  }
 
-        const [bufferA, bufferB] = await Promise.all([
-            this.fetchAudioBuffer(trackAUrl),
-            this.fetchAudioBuffer(trackBUrl)
-        ])
+  pause() {
+    if (!this.isPlaying) return
 
-        console.log('Buffers loaded: ', { bufferA, bufferB })
+    this.sourceA?.stop()
+    this.sourceB?.stop()
 
-        this.audioBufferA = bufferA
-        this.audioBufferB = bufferB
-    }
+    this.offset = this.audioContext.currentTime - this.startTime
+    this.isPlaying = false
+  }
 
-    protected getLoadedTracks(): LoadedTracks {
-        return {
-            trackA: this.audioBufferA,
-            trackB: this.audioBufferB
-        }
-    }
-
-    protected getSourceNodes() {
-        return {
-            sourceA: this.sourceA,
-            sourceB: this.sourceB
-        }
-    }
-
-    private async fetchAudioBuffer(url: string): Promise<AudioBuffer> {
-        /**
-         * Fetches and decodes track
-         */
-        console.log('Fetching: ', url)
-        const response = await fetch(url)
-        const arrayBuffer = await response.arrayBuffer()
-        return await this.audioContext.decodeAudioData(arrayBuffer)
-    }
-
-    play() {
-
-        // If we're already playing or either buffer is empty, return
-        if (this.isPlaying) return
-        if (!this.audioBufferA || !this.audioBufferB) return
-
-        this.sourceA = this.audioContext.createBufferSource()
-        this.sourceB = this.audioContext.createBufferSource()
-
-
-        this.sourceA.buffer = this.audioBufferA
-        this.sourceB.buffer = this.audioBufferB
-
-        this.sourceA.connect(this.gainA)
-        this.sourceB.connect(this.gainB)
-
-        this.startTime = this.audioContext.currentTime - this.offset
-        this.sourceA.start(0, this.offset)
-        this.sourceB.start(0, this.offset)
-
-        this.isPlaying = true
-
-    }
-
-    pause() {
-        if (!this.isPlaying) return
-
-        this.sourceA?.stop()
-        this.sourceB?.stop()
-
-        this.offset = this.audioContext.currentTime - this.startTime
-        this.isPlaying = false
-    }
-
-    setBalance(value: number) {
-        this.gainA.gain.value = 1 - value
-        this.gainB.gain.value = value
-        console.log(`Gain A: ${this.gainA.gain.value}, Gain B: ${this.gainB.gain.value}`)
-    }
-
-
-
+  setBalance(value: number) {
+    this.gainA.gain.value = 1 - value
+    this.gainB.gain.value = value
+    console.log(`Gain A: ${this.gainA.gain.value}, Gain B: ${this.gainB.gain.value}`)
+  }
 }
